175cc28eeda8bf611ae08f7161722739d769741d
scadler <scadler@s.sfusd.edu>
2020-01-19 20:23:38 +0000

    torpedo number gens created

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ $(document).mousemove(function(e){
 
 var $loop = setInterval(function(){
 // change 12 to alter damping higher is slower
-$xp += (($mouseX - $xp - 25)/12);
-$yp += (($mouseY - $yp -25)/12);
+$xp += (($mouseX - $xp - 25)/20);
+$yp += (($mouseY - $yp -25)/20);
 $("#ship").css({left:$xp +'px', top:$yp +'px'});  
 }, 30);
+
+setInterval(function(){
+    var row = Math.floor(Math.random()*9)+1
+    var speed = (Math.floor(Math.random()*2)+1)*8
+    console.log(row+", "+speed)
+
+}, 1500)
