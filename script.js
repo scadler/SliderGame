e2d5f66a84d55f644f11032ded955aad00268bc9
scadler <scadler@s.sfusd.edu>
2020-01-19 23:37:25 +0000

    torpedos work, now working on hit detection

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ $("#ship").css({left:$xp +'px', top:$yp +'px'});
 
 setInterval(function(){
     var row = Math.floor(Math.random()*9)+1
-    var speed = (Math.floor(Math.random()*2)+1)*8
+    var speed = (Math.floor(Math.random()*14)+1)+15
     console.log(row+", "+speed)
-
-}, 1500)
+    if($("#div"+row).contents().hasClass('torpedo')=== false){
+    $("#div"+row).append(`<marquee class="torpedo" id="torpedo${row}" loop="1" scrollamount=${speed}><p class="img" id="img${row}"> torpedo</p> </marquee>`);
+    }
+    else{
+        var x = $("#img"+row).offset().left
+        console.log(x)
+        if(x <= -35){
+            $("#torpedo"+row).remove();
+            $("#div"+row).append(`<marquee class="torpedo" id="torpedo${row}" loop="1" scrollamount=${speed}><p class="img" id="img${row}"> torpedo</p> </marquee>`);
+        }
+    }
+}, 500)
